6a4d1a52c64b1fc0a94a4bbe4dac5c628dbf7756
AIUniversalWealth <zrkhan0942@gmail.com>
2025-03-23 23:51:48 +0100

    Create Dashboard.js

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>Willkommen auf dem Dashboard!</h2>
+      <p>Du bist erfolgreich eingeloggt.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
