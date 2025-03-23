17dcece5d95d85fd148d3a02a02b03d8a3e7360d
AIUniversalWealth <zrkhan0942@gmail.com>
2025-03-23 12:45:17 +0100

    Add redirect from / to /login

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login.js";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
